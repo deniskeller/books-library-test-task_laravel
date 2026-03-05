1335bd5016e50c7fa321d62678c4025c9c80dc44
deniskeller <dentean@mail.ru>
2026-03-05 22:45:00 +0400

    feat: add start routing

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.welcome');
-});
+})->middleware('auth');
 
 Route::get('/login', function () {
     return view('pages.login');
-});
+})->middleware('guest')->name('login');
 
-Route::post('/login', function () {
-    return view('pages.login');
-});
+// Route::post('/login', function () {
+//     return view('pages.login');
+// });
