d4b5cbe200a7a488d9244101435472c69a5c3930
deniskeller <dentean@mail.ru>
2026-03-10 23:34:12 +0400

    feat: make StoreAuthorRequest

---
+++ app/Http/Requests/StoreAuthorRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\NoHtmlTags;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAuthorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:2', 'max:255', new NoHtmlTags()],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Поле обязательно для заполнения',
+            'name.min' => 'Имя автора должно состоять минимум из 2 символов',
+            'name.max' => 'Имя автора должно состоять максимум из 255 символов',
+            'name.regex' => 'HTML-теги запрещены',
+        ];
+    }
+
+
+    public function attributes(): array
+    {
+        return [
+            'name' => 'имя автора',
+        ];
+    }
+}
