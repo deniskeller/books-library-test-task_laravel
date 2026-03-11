c6cce266fad59c336f738a28d344a70c0da29264
deniskeller <dentean@mail.ru>
2026-03-11 17:51:18 +0400

    feat: create group routs, create AdminMiddleware

--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,61 @@ Route::get('/', function () {
     return view('pages.welcome');
 })->name('welcome');
 
-// роуты авторизацции
-Route::get('/login', [LoginController::class, 'show'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+// // роуты авторизацции
+// Route::get('/login', [LoginController::class, 'show'])->name('login');
+// Route::post('/login', [LoginController::class, 'login']);
+// Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-// роуты регистрации
-Route::get('/register', [RegisterController::class, 'show'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
+// // роуты регистрации
+// Route::get('/register', [RegisterController::class, 'show'])->name('register');
+// Route::post('/register', [RegisterController::class, 'register']);
 
-// роуты книг
-Route::get('/books', [BookController::class, 'index'])->name('books.index'); // страница книг
-Route::get('/books/create', [BookController::class, 'create'])->name('books.create'); // страница создания новой книги
-Route::post('/books', [BookController::class, 'store'])->name('books.store'); // сохранение новой книги
-Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit'); // страница редактирования книги
-Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update'); // сохранение отредактированной книги
-Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy'); // удаления книги
+// // роуты книг
+// Route::get('/books', [BookController::class, 'index'])->name('books.index'); // страница книг
+// Route::get('/books/create', [BookController::class, 'create'])->name('books.create'); // страница создания новой книги
+// Route::post('/books', [BookController::class, 'store'])->name('books.store'); // сохранение новой книги
+// Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit'); // страница редактирования книги
+// Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update'); // сохранение отредактированной книги
+// Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy'); // удаления книги
 
-Route::get('books/{book}/category/{category}', [BookController::class, 'show']); // тестовый роут для нескольких параметров
+// Route::get('books/{book}/category/{category}', [BookController::class, 'show']); // тестовый роут для нескольких параметров
 
-// роуты авторов
-Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index'); // страница авторов
-Route::get('/authors/create', [AuthorController::class, 'create'])->name('authors.create'); // страница создания новой авторов
-Route::post('/authors', [AuthorController::class, 'store'])->name('authors.store'); // сохранение новой авторов
-Route::get('/authors/{author}/edit', [AuthorController::class, 'edit'])->name('authors.edit'); // страница редактирования авторов
-Route::put('/authors/{author}', [AuthorController::class, 'update'])->name('authors.update'); // сохранение отредактированной авторов
-Route::delete('/authors/{author}', [AuthorController::class, 'destroy'])->name('authors.destroy'); // удаления автора
+// // роуты авторов
+// Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index'); // страница авторов
+// Route::get('/authors/create', [AuthorController::class, 'create'])->name('authors.create'); // страница создания новой авторов
+// Route::post('/authors', [AuthorController::class, 'store'])->name('authors.store'); // сохранение новой авторов
+// Route::get('/authors/{author}/edit', [AuthorController::class, 'edit'])->name('authors.edit'); // страница редактирования авторов
+// Route::put('/authors/{author}', [AuthorController::class, 'update'])->name('authors.update'); // сохранение отредактированной авторов
+// Route::delete('/authors/{author}', [AuthorController::class, 'destroy'])->name('authors.destroy'); // удаления автора
+
+Route::middleware('guest')->group(function () {
+    // роуты авторизацции
+    Route::get('/login', [LoginController::class, 'show'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+    // роуты регистрации
+    Route::get('/register', [RegisterController::class, 'show'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    // роуты книг
+    Route::get('/books', [BookController::class, 'index'])->name('books.index'); // страница книг
+    Route::get('/books/create', [BookController::class, 'create'])->name('books.create'); // страница создания новой книги
+    Route::post('/books', [BookController::class, 'store'])->name('books.store'); // сохранение новой книги
+    Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit'); // страница редактирования книги
+    Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update'); // сохранение отредактированной книги
+
+    // роуты авторов
+    Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index'); // страница авторов
+    Route::get('/authors/create', [AuthorController::class, 'create'])->name('authors.create'); // страница создания новой авторов
+    Route::post('/authors', [AuthorController::class, 'store'])->name('authors.store'); // сохранение новой авторов
+    Route::get('/authors/{author}/edit', [AuthorController::class, 'edit'])->name('authors.edit'); // страница редактирования авторов
+    Route::put('/authors/{author}', [AuthorController::class, 'update'])->name('authors.update'); // сохранение отредактированной авторов
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy'); // удаления книги
+    Route::delete('/authors/{author}', [AuthorController::class, 'destroy'])->name('authors.destroy'); // удаления автора
+});
